4ca3819077c40be743c48763ceab84a25a5cce4b
Tyler-Stocks <tylerjstocks@gmail.com>
2024-09-07 17:33:08 -0700

    Began cleaning up AprilTagTuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/apriltags/AprilTagTuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/apriltags/AprilTagTuning.java
@@ -2,20 +2,24 @@ package org.firstinspires.ftc.teamcode.vision.apriltags;
 
 import android.util.Size;
 
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.RunCommand;
+import com.arcrobotics.ftclib.command.button.GamepadButton;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.qualcomm.robotcore.eventloop.opmode.*;
-import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.*;
-import org.firstinspires.ftc.robotcore.internal.files.DataLogger;
+import org.firstinspires.ftc.teamcode.utility.DataLogger;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.*;
+import static org.firstinspires.ftc.teamcode.constants.Constants.AprilTagConstants.*;
 
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 @TeleOp(name = "Utility - April Tag Tuning", group = "Utility")
-public class AprilTagTuning extends OpMode {
+public class AprilTagTuning extends CommandOpMode {
     private AprilTagProcessor aprilTagProcessor;
     private DataLogger aprilTagLogger;
 
@@ -24,37 +28,26 @@ public class AprilTagTuning extends OpMode {
     private VisionPortal visionPortal;
 
     private int minExposure, maxExposure, exposure;
-
     private int minGain, gain, maxGain;
-
     private int minWhiteBalance, whiteBalance, maxWhiteBalance;
 
-    private Gamepad currentGamepad, prevGamepad;
+    @Override public void initialize() {
+        initializeAprilTagDetection();
+        initializeCamera();
 
-    @Override public void init() {
-        initAprilTagDetection();
-        getDefaultCameraSettings();
+        aprilTagLogger = new DataLogger("AprilTagLogFile");
 
-        aprilTagLogger = new DataLogger("AprilTagLogger");
-
-        exposure = 2;
-        gain = 0;
-        whiteBalance = 4000;
-
-        setCameraProperties(exposure, gain, whiteBalance);
-
-        prevGamepad    = new Gamepad();
-        currentGamepad = new Gamepad();
+        schedule(
+                new RunCommand(this::updateDetections),
+                new RunCommand(telemetry::update)
+        );
     }
 
-    @Override public void loop() {
-        prevGamepad.copy(currentGamepad);
-        currentGamepad.copy(gamepad1);
+    private void configureBindings() {
+        GamepadEx driverGamepad = new GamepadEx(gamepad1);
+    }
 
-        telemetry.addData("Exposure", exposure);
-        telemetry.addData("Gain", gain);
-        telemetry.addData("White Balance", whiteBalance);
-
+    private void updateDetections() {
         List<AprilTagDetection> currentDetections = aprilTagProcessor.getDetections();
 
         int numberOfTags = currentDetections.size();
@@ -68,8 +61,8 @@ public class AprilTagTuning extends OpMode {
         for (AprilTagDetection detection : currentDetections) {
             if (detection.metadata == null) continue;
 
-            if ((DESIRED_TAG_ID < 0) || (detection.id == DESIRED_TAG_ID)) {
-                allAprilTagDetections.add(detection);
+            if (DESIRED_TAG_ID < 0 || detection.id == DESIRED_TAG_ID) {
+                aprilTagLogger.addLine(detectionAsString(detection));
 
                 telemetry.addData("Detected Tag", detection.id);
                 telemetry.addData("Distance From Tag", detection.ftcPose.range);
@@ -78,27 +71,16 @@ public class AprilTagTuning extends OpMode {
                 telemetry.addData("Roll", detection.ftcPose.roll);
             }
         }
-
-        listenForCameraPropertyAdjustments();
-
-        exposure = Range.clip(exposure, minExposure, maxExposure);
-        gain = Range.clip(gain, minGain, maxGain);
-        whiteBalance = Range.clip(whiteBalance, minWhiteBalance, maxWhiteBalance);
-
-        setCameraProperties(exposure, gain, whiteBalance);
-
-        telemetry.update();
-
-        if(gamepad1.options){
-            saveAprilTagData();
-        }
     }
 
-    @Override public void stop() {
 
+    private String detectionAsString(AprilTagDetection detection) {
+       return System.currentTimeMillis() + "," + detection.ftcPose.yaw + ","
+              + detection.ftcPose.pitch + "," + detection.ftcPose.roll + ","
+              + detection.ftcPose.range + "," + detection.ftcPose.bearing;
     }
 
-    private void initAprilTagDetection() {
+    private void initializeAprilTagDetection() {
         aprilTagProcessor = new AprilTagProcessor.Builder()
                 .setDrawTagOutline(true)
                 .setLensIntrinsics(660.750, 660.75, 323.034, 230.681) // C615 measured kk Dec 5 2023
@@ -112,68 +94,59 @@ public class AprilTagTuning extends OpMode {
                 .build();
     }
 
-    private void getDefaultCameraSettings() {
-        while (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) { // Wait for camera to start streaming
+    private void initializeCamera() {
+        while (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) {
             telemetry.addLine("Waiting for camera");
             telemetry.update();
         }
+
+        // Ensure telemetry is updated properly after the loop
+        telemetry.update();
 
         ExposureControl exposureControl = visionPortal.getCameraControl(ExposureControl.class);
 
         minExposure = (int) exposureControl.getMinExposure(TimeUnit.MILLISECONDS) + 1;
         maxExposure = (int) exposureControl.getMaxExposure(TimeUnit.MILLISECONDS);
+        exposure    = Range.clip(EXPOSURE_MS, minExposure, maxExposure);
 
         GainControl gainControl = visionPortal.getCameraControl(GainControl.class);
 
         minGain = gainControl.getMinGain();
         maxGain = gainControl.getMaxGain();
+        gain    = Range.clip(GAIN, minGain, maxGain);
 
-        WhiteBalanceControl whiteBalanceControl = visionPortal.getCameraControl(WhiteBalanceControl.class);
+        WhiteBalanceControl whiteBalanceControl
+                = visionPortal.getCameraControl(WhiteBalanceControl.class);
 
         minWhiteBalance = whiteBalanceControl.getMinWhiteBalanceTemperature();
         maxWhiteBalance = whiteBalanceControl.getMaxWhiteBalanceTemperature();
-        whiteBalance = whiteBalanceControl.getWhiteBalanceTemperature();
+        whiteBalance    = Range.clip(WHITE_BALANCE, minWhiteBalance, maxWhiteBalance);
     }
 
-    private void listenForCameraPropertyAdjustments() {
-        if (currentGamepad.left_bumper && !prevGamepad.left_bumper) exposure++;
-
-        if (currentGamepad.left_trigger > 0.5 && !(prevGamepad.left_trigger > 0.5)) exposure--;
-
-        if (currentGamepad.right_bumper && !prevGamepad.right_bumper) gain++;
-
-        if (currentGamepad.right_trigger > 0.5 && !(prevGamepad.right_trigger > 0.5)) gain--;
-
-        if (currentGamepad.dpad_up && !prevGamepad.dpad_up) whiteBalance++;
-
-        if (currentGamepad.dpad_down && !prevGamepad.dpad_down) whiteBalance--;
-
-        if (currentGamepad.dpad_left && !prevGamepad.dpad_left) whiteBalance += 1;
-
-        if (currentGamepad.dpad_right && !prevGamepad.dpad_right) whiteBalance -= 10;
-    }
-
-    private void setCameraProperties(int exposureMS, int gain, int white) {
+    private void setCameraProperties(int exposureMS, int gain, int whiteBalance) {
         // Wait for camera to start streaming
         while (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) {
             telemetry.addLine("Waiting for camera");
             telemetry.update();
         }
 
-        // Make sure that the telemetry clears properly after we exit the while loop
+        // Ensure telemetry clears properly after we exit the while loop
         telemetry.update();
 
         ExposureControl exposureControl = visionPortal.getCameraControl(ExposureControl.class);
-
         exposureControl.setMode(ExposureControl.Mode.Manual);
-        exposureControl.setExposure(exposureMS, TimeUnit.MILLISECONDS);
+        exposureControl.setExposure(
+                Range.clip(exposureMS, minExposure, maxExposure),
+                TimeUnit.MILLISECONDS
+        );
 
         GainControl gainControl = visionPortal.getCameraControl(GainControl.class);
-        gainControl.setGain(gain);
+        gainControl.setGain(Range.clip(gain, minGain, maxGain));
 
-        WhiteBalanceControl whiteBalanceControl = visionPortal.getCameraControl(WhiteBalanceControl.class);
-
+        WhiteBalanceControl whiteBalanceControl
+                = visionPortal.getCameraControl(WhiteBalanceControl.class);
         whiteBalanceControl.setMode(WhiteBalanceControl.Mode.MANUAL);
-        whiteBalanceControl.setWhiteBalanceTemperature(white);
+        whiteBalanceControl.setWhiteBalanceTemperature(
+                Range.clip(whiteBalance, minWhiteBalance, maxWhiteBalance));
     }
 }
